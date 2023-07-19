a827b2cbd4a3585b95ec097e573f300970105ff2
Oleh Dashkevych <skoner@ukr.net>
2023-07-19 22:14:59 +0300

    finished task

---
+++ controllers/contacts/index.js
@@ -0,0 +1,16 @@
+const { ctrlWrapper } = require('../../helpers');
+const getAll = require('./getAll');
+const getById = require('./getById');
+const add = require('./add');
+const updateById = require('./updateById');
+const updateFavorite = require('./updateFavorite');
+const deleteById = require('./deletedById');
+
+module.exports = {
+    getAll: ctrlWrapper(getAll),
+    getById: ctrlWrapper(getById),
+    add: ctrlWrapper(add),
+    updateById: ctrlWrapper(updateById),
+    updateFavorite: ctrlWrapper(updateFavorite),
+    deleteById: ctrlWrapper(deleteById),
+};
